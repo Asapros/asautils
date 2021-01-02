1128f46409f6d080d0a588e874adacc61c290e2f
Asapros <asapros@wp.pl>
2021-01-02 16:12:28 +0100

    trying to upload to pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+#
+from setuptools import setup
+
+setup(name="asautils",
+      author="Asapros",
+      url="https://github.com/Asapros/asautils",
+      version="0.0.1",
+      description="Useful features",
+      py_modules=["extypes", "gamebase"],
+      package_dir={"":"src"}
+      )
