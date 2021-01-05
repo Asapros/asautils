ca3d3fef64c2896712622cddbbe0bd9dcd7e250c
Asapros <asapros@wp.pl>
2021-01-05 16:22:34 +0100

    Updated installing modules

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # asautils
 Package with some useful features
+1. Rename 'installconfig_template.json' to 'installconfig.json' and edit it with correct path
+2. Run 'install.py'
--- install.py
+++ install.py
@@ -1,19 +1,27 @@
 #
 import os
 from shutil import copy
+import time
 
-print("[install.py] Setting up...")
+
+start = time.time()
+print("Setting up...")
 try:
     from json import load
     config = load(open("installconfig.json", "r"))
     DIR = config["dir"]
-    DISTDIR = config["dist"]
+    DISTDIR = config["dist"] + "asautils//"
 except Exception as e:
     input(e)
     exit()
 
-print("[install.py] Copying modules...")
+print("Copying modules...")
 for module in os.listdir(DIR):
     if module.endswith("py"):
-        copy(DIR + module, DISTDIR)
-print("[install.py] Copied module files from %s to %s" % (DIR, DISTDIR))
+        try:
+            copy(DIR + module, DISTDIR)
+        except (FileNotFoundError, PermissionError) as e:
+            input(e)
+            exit()
+print("Copied module files from %s to %s" % (DIR, DISTDIR))
+input("Completed in " + str(time.time() - start))
