6601046ee730b9c40260f5aed618c57d37b1047a
Asapros <asapros@wp.pl>
2021-01-22 14:32:03 +0100

    Added docs to consoleui

--- src/consoleui.py
+++ src/consoleui.py
@@ -1,17 +1,33 @@
-#
+"""
+Some tools to make command line apps lot more user-friendly
+
+Examples:
+    OptionSelctor(["option1", "option2", "option3"], title="SELECT AN OPTION:").run()
+    ->
+
+SELECT AND OPTION:
+option1 <
+option2
+option3
+
+"""
 from enum import Enum
 from pynput import keyboard
 from os import system
 
 class Sides(Enum):
+    """Enum used for specifying sides of cursor in OptionSelector"""
     LEFT = 0 
     RIGHT = 1
     BOTH = 2
     # There will never be 'center' side cause it's too glitchy
 
 
-def clear(): system("cls")
+def clear():
+    """Clearing cmd screen"""
+    system("cls")
 def genText(text, x, y, anchor = Sides.LEFT):
+    """Generating text in specific place cmd"""
     if anchor == Sides.LEFT:
         return "\033[{2};{1}H{0}".format(text, x, y)
     elif anchor == Sides.RIGHT:
@@ -19,6 +35,13 @@ def genText(text, x, y, anchor = Sides.LEFT):
 
 class OptionSelector:
     def __init__(self, options, selectchars = (">","<"), space=1, title="", selectside = Sides.RIGHT, footer=""):
+        """OptionSelctor is used for creating cmd menus. Addicional arguments:
+    title, footer - text that will appear before, and after options.
+    space         - space between selected text and selectchar
+    selectside    - specify what side do you want selecting arrows to be
+    selectchars   - specify what chars will appear before and after selected option
+    
+"""
         self.options = options
         self.selected = 0
         self.space = space
@@ -27,6 +50,8 @@ class OptionSelector:
         self.title = title
         self.footer = "\n" + footer
     def run(self):
+        """OptionSelector can be runned infinite amount of times.
+run() will block the main thread, and return index of selected option"""
         clear()
         self.keylistener = keyboard.Listener(on_press=self.__keypressed)
         self.keylistener.start()
@@ -35,6 +60,9 @@ class OptionSelector:
         clear()
         return self.selected
     def update(self):
+        """This method is not private only in case if you'll want to change
+options, title, footer or other while selecting is running. REMEMBER - run() will block main thread,
+you'll need to run your code in other one."""
         optionadder = 1
         updatestr = ""
         if self.title != "":
@@ -54,12 +82,12 @@ class OptionSelector:
                     selectedA = self.selectchars[0] + " " * self.space
             else:
                 if self.selectside == Sides.RIGHT:
-                    selectedB = " " * self.space + " "
+                    selectedB = " " * self.space + " " * len(self.selectchars[0])
                 elif self.selectside == Sides.LEFT:
-                    selectedA = " " * self.space + " "
+                    selectedA = " " * self.space + " " * len(self.selectchars[1])
                 elif self.selectside == Sides.BOTH:
-                    selectedB = " " * self.space + " "
-                    selectedA = " " * self.space + " "
+                    selectedB = " " * self.space + " " * len(self.selectchars[1])
+                    selectedA = " " * self.space + " " * len(self.selectchars[0])
 
             updatestr += genText(selectedA + self.options[option] + selectedB, 1, option+optionadder)
         updatestr += self.footer
