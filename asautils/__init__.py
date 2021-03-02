bc9e317b37301a19c38f08f901a1dda439a7a4fa
Asapros <asapros@wp.pl>
2021-03-02 14:51:33 +0100

    Added init

---
+++ asautils/__init__.py
@@ -0,0 +1,14 @@
+"""
+This is asautils package by Asapros
+I'm not going to put more information there,
+you can checkout README.md, setup.py and LICENSE
+on my repo: https://github.com/Asapros/asautils/
+"""
+
+from asautils import consoleui
+from asautils import extypes
+from asautils import gamebase
+from asautils import logger
+__all__ = [
+    "consoleui", "extypes", "gamebase", "logger"
+    ]
