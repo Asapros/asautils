9aad17b9a451c277f67e65ad264827522ab6a418
Asapros <asapros@wp.pl>
2021-03-02 14:09:43 +0100

    Rewrited OptionSelector in curses

--- src/consoleui.py
+++ src/consoleui.py
@@ -11,11 +11,12 @@ option2
 option3
 
 """
-from enum import Enum
-from os import system
-from pynput import keyboard
-from time import sleep
-from random import randint
+from enum     import Enum
+from os       import system
+from time     import sleep
+from random   import randint
+from colorama import Fore
+import curses
 
 class Sides(Enum):
     """Enum used for specifying sides of cursor in OptionSelector"""
@@ -24,16 +25,7 @@ class Sides(Enum):
     BOTH = 2
     # There will never be 'center' side cause it's too glitchy
 
-
-def clear():
-    """Clearing cmd screen"""
-    system("cls")
-def gen_text(text, x, y, anchor = Sides.LEFT):
-    """Generating text in specific place cmd"""
-    if anchor == Sides.LEFT:
-        return "\033[{2};{1}H{0}".format(text, x, y)
-    elif anchor == Sides.RIGHT:
-        return "\033[{2};{1}H{0}".format(text, x-len(text)+1, y)
+def format_colors(string): return string.format(green=Fore.GREEN, reset=Fore.RESET, red=Fore.RED, blue=Fore.BLUE, cyan=Fore.CYAN, yellow=Fore.YELLOW, magenta=Fore.MAGENTA)
 
 def tprint(text, mintime, maxtime, end="\n"):
     """Slowly printing text.
@@ -43,6 +35,7 @@ mintime and maxtime are in miliseconds
         print(char, end="", flush=True)
         sleep(randint(mintime, maxtime)/1000)
     print(end, end="")
+
 def type_print(text, end="\n"):
     """Simulates text typed by someone"""
     for char in text:
@@ -63,68 +56,70 @@ class OptionSelector:
     selectchars   - specify what chars will appear before and after selected option
     
 """
+        if space < 1: space = 1
         self.options = options
         self.selected = 0
         self.space = space
         self.selectside = selectside
         self.selectchars = selectchars
         self.title = title
-        self.footer = "\n" + footer
+        self.footer = footer
     def run(self):
         """OptionSelector can be runned infinite amount of times.
 run() will block the main thread, and return index of selected option"""
-        clear()
-        self.keylistener = keyboard.Listener(on_press=self.__keypressed)
-        self.keylistener.start()
-        self.update()
-        self.keylistener.join()
-        clear()
+        try: curses.initscr()
+        except AttributeError:
+            print("You can't open it from IDE!")
+            return
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        curses.wrapper(self.__key_listening)
+        curses.endwin()
         return self.selected
-    def update(self):
-        """This method is not private only in case if you'll want to change
-options, title, footer or other while selecting is running. REMEMBER - run() will block main thread,
-you'll need to run your code in other one."""
-        optionadder = 1
+    def __key_listening(self, screen):
+        while True:
+            screen.clear()
+            screen.addstr(self.string())
+            key = screen.getch()
+            screen.refresh()
+            if key == curses.KEY_UP:
+                self.selected = self.__limited(False, self.selected, len(self.options)-1)
+            elif key == curses.KEY_DOWN:
+                self.selected = self.__limited(True, self.selected, len(self.options)-1)
+            elif key in (curses.KEY_ENTER, 10, 13):
+                break
+    def string(self):
+        """To string method"""
         updatestr = ""
         if self.title != "":
-            optionadder = 2
-            updatestr = gen_text(self.title, 1, 1) 
+            updatestr += self.title + "\n"
+        padding = ["", ""]
+        selected_padding = ["", ""]
+        if self.selectside in (Sides.RIGHT, Sides.BOTH):
+            padding[1] = " "*self.space
+            selected_padding[1] = " "*(self.space-1) + self.selectchars[1]
+        if self.selectside in (Sides.LEFT, Sides.BOTH):
+            padding[0] = " "*self.space
+            selected_padding[0] = " "*(self.space-1) + self.selectchars[0]
+        
 
-        for option in range(0,len(self.options)):
-            selectedA = ""
-            selectedB = ""
-            if self.selected == option:
-                if self.selectside == Sides.RIGHT:
-                    selectedB = " "*self.space + self.selectchars[1]
-                elif self.selectside == Sides.LEFT:
-                    selectedA = self.selectchars[0] + " " * self.space
-                elif self.selectside == Sides.BOTH:
-                    selectedB = " " * self.space + self.selectchars[1]
-                    selectedA = self.selectchars[0] + " " * self.space
+        def wrap(selected, option):
+            if selected:
+                return selected_padding[0] + option + selected_padding[1]
             else:
-                if self.selectside == Sides.RIGHT:
-                    selectedB = " " * self.space + " " * len(self.selectchars[0])
-                elif self.selectside == Sides.LEFT:
-                    selectedA = " " * self.space + " " * len(self.selectchars[1])
-                elif self.selectside == Sides.BOTH:
-                    selectedB = " " * self.space + " " * len(self.selectchars[1])
-                    selectedA = " " * self.space + " " * len(self.selectchars[0])
+                return padding[0] + option + padding[1]
+        
+        for option in range(0,len(self.options)):
+            if option == self.selected:
+                updatestr += wrap(True, self.options[option]) + "\n"
+            else:
+                updatestr += wrap(False, self.options[option]) + "\n"
+            
 
-            updatestr += gen_text(selectedA + self.options[option] + selectedB, 1, option+optionadder)
+
         updatestr += self.footer
-        print(updatestr)
-    def __keypressed(self, key):
-        if "char" in dir(key):
-            pass
-        else:
-            if key == keyboard.Key.enter:
-                input()
-                return False
-            elif key == keyboard.Key.up:
-                self.selected = self.__limited(False, self.selected, len(self.options)-1)
-            elif key == keyboard.Key.down:
-                self.selected = self.__limited(True, self.selected, len(self.options)-1)
-            self.update()
+        return updatestr
     def __limited(self, operation, variable, limit):
         if operation:
             if variable >= limit:
