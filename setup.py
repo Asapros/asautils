dcae06ca8e85d45e7fcec401104fdb72dd7c8886
Asapros <asapros@wp.pl>
2021-03-07 15:53:11 +0100

    Modified setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
    name='asautils',
    version='1.0',
-   description='A useful',
+   description='A useful python package',
    author='Asapros',
    packages=['asautils'],
    install_requires=['colorama', 'windows-curses']
