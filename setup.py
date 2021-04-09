b43d3c77bc6cbdcd29a7d385041abdcc7ec4ac39
Asapros <asapros@wp.pl>
2021-04-09 16:59:18 +0200

    Added super smart support for other systems

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
+from platform import system
+
+curses_name = "curses"
+if system() == "Windows": curses_name = "windows-curses"
+
 setup(
    name='asautils',
    version='1.0',
    description='A useful python package',
    author='Asapros',
    packages=['asautils'],
-   install_requires=['colorama', 'windows-curses']
+   install_requires=['colorama', curses_name]
 )
