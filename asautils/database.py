3a67861453eaf30d87b296822244e991dba5da95
Asapros <asapros@wp.pl>
2021-06-26 17:52:17 +0200

    Minor changes, added documentation

--- asautils/database.py
+++ asautils/database.py
@@ -9,7 +9,8 @@ class Database:
         self.cursor = None
         self.tables = {}
 
-    def __require(connection=False, cursor=False):
+    def _require(connection: bool = False, cursor: bool = False):
+        """Wrapper not allowing wrapped methods to be called when connection/cursor is not assigned"""
         def inner(func):
             def wrapper(self, *args, **kwargs):
                 if (connection and self.connection is None) or (cursor and self.cursor is None):
@@ -19,21 +20,25 @@ class Database:
         return inner
     
     def connect(self):
+        """Connecting to the database"""
         self.connection = connect(self.file)
         self.cursor = self.connection.cursor()
 
-    @__require(connection=True)
+    @_require(connection=True)
     def disconnect(self):
+        """Disconnecting from database"""
         self.connection.close()
         self.connection = None
         self.cursor = None
 
-    @__require(connection=True)
+    @_require(connection=True)
     def commit(self):
+        """Commit changes"""
         self.connection.commit()
 
-    @__require(cursor=True)
-    def create_table(self, name: str, model_class: dataclass, options = None) -> None:
+    @_require(cursor=True)
+    def create_table(self, name: str, model_class: dataclass, options = None):
+        """Create a table"""
         template = """CREATE TABLE {}({})"""
         columns = ",".join([" ".join([varname, vartype]) for varname, vartype in zip(model_class.__annotations__.keys(), model_class.__annotations__.values())])
         content = columns if options is None else ",".join([columns, options])
@@ -41,15 +46,17 @@ class Database:
         self.cursor.execute(query)
         self.tables[name] = model_class
 
-    @__require(cursor=True)
+    @_require(cursor=True)
     def insert(self, table: str, model: dataclass):
+        """Insert a model to a table"""
         template = """INSERT INTO {} VALUES ({})"""
-        query = template.format(table, ",".join(["?"]*len(model.__annotations__.keys())))
-        values = [vars(model)[key] for key in model.__annotations__.keys()]
+        query = template.format(table, ",".join(["?"]*len(model.__dataclass_fields__.keys())))
+        values = [vars(model)[key] for key in model.__dataclass_fields__.keys()]
         return self.cursor.execute(query, values)
 
-    @__require(cursor=True)
-    def select(self, table: str, columns: list, condition: str = None):
+    @_require(cursor=True)
+    def select(self, table: str, columns: list = ["*"], condition: str = None):
+        """Fetch rows from a table. When there are too less columns to create a model, SimpleNamespace will be created instead"""
         model_class = self.tables.get(table, SimpleNamespace)
         template = """SELECT {} FROM {}{}"""
         query = template.format(",".join(columns), table, " WHERE {}".format(condition) if condition is not None else "")
@@ -73,14 +80,16 @@ class Database:
 
         return models
 
-    @__require(cursor=True)
+    @_require(cursor=True)
     def delete(self, table: str, condition: str = None):
+        """Delete from a table"""
         template = """DELETE FROM {}{}"""
         query = template.format(table, " WHERE {}".format(condition) if condition is not None else "")
         self.cursor.execute(query)
 
-    @__require(cursor=True)
+    @_require(cursor=True)
     def update(self, table: str, values: dict, condition: str = None):
+        """Changes values in the table."""
         template = """UPDATE {} SET {}{}"""
         update_values = ",".join(["{} = ?".format(key) for key in values.keys()])
         query = template.format(table, update_values, " WHERE {}".format(condition) if condition is not None else "")
